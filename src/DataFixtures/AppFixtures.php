def4de4eb971b91050f50c6bf1e43f37ee579f70
Riot Nicolas <nico.riot@free.fr>
2020-02-28 17:06:50 +0100

    creation premiere entity + fixture

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Post;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $faker = Factory::create(); // pb ?
+
+        for($i = 1 ; $i <=5 ; $i++){
+            $post = new Post();
+            $post->setTitle($faker->sentence())
+                ->setSubtitle($faker->sentence())
+                ->setContent($faker->paragraph())
+                ->setCreatedAt($faker->dateTimeAD($max = 'now', $timezone = null));
+
+
+        }
+        $manager->persist($post);
+        $manager->flush();
+    }
+}
