bdeaa6d193dc3ab506e395e0b0cbd2f7d32b6670
Riot Nicolas <nico.riot@free.fr>
2020-03-03 19:29:14 +0100

    table users, correction bug

--- src/Controller/SiteController.php
+++ src/Controller/SiteController.php
@@ -3,79 +3,67 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SiteController extends AbstractController
 {
     /**
+     * this->render methode de abstractController retourn vue de type reponse
      * @Route("/", name="site")
      */
-    public function index()
+    public function index():Response
     {
-        return $this->render('site/index.html.twig', [
-            'controller_name' => 'SiteController',
-        ]);
+        return $this->render('site/index.html.twig');
     }
 
 
     /**
      * @Route("/trail", name="trail")
      */
-    public function trail()
+    public function trail():response
     {
-        return $this->render('post/trail.html.twig', [
-            'controller_name' => 'SiteController',
-        ]);
+        return $this->render('post/trail.html.twig');
     }
 
     /**
      * @Route("/treck", name="treck")
      */
-    public function treck()
+    public function treck():response
     {
-        return $this->render('post/treck.html.twig', [
-            'controller_name' => 'SiteController',
-        ]);
+        return $this->render('post/treck.html.twig');
     }
 
     /**
      * @Route("/vtt", name="vtt")
      */
-    public function vtt()
+    public function vtt():response
     {
-        return $this->render('post/vtt.html.twig', [
-            'controller_name' => 'SiteController',
-        ]);
+        return $this->render('post/vtt.html.twig');
     }
 
 
     /**
      * @Route("/actu", name="actu")
      */
-    public function actu()
+    public function actu():response
     {
-        return $this->render('post/actu.html.twig', [
-            'controller_name' => 'SiteController',
-        ]);
+        return $this->render('post/actu.html.twig');
     }
 
     /**
      * @Route("/evenement", name="event")
      */
-    public function event()
+    public function event():response
     {
-        return $this->render('site/event.html.twig', [
-            'controller_name' => 'SiteController',
-        ]);
+        return $this->render('site/event.html.twig');
     }
 
     /**
      * @Route("/adhesion", name="adhesion")
      */
-    public function adhesion()
+    public function adhesion():response
     {
-        return $this->render('site/adhesion.html.twig', [
-            'controller_name' => 'SiteController',
-        ]);
+        return $this->render('site/adhesion.html.twig');
     }
 }
---
+++ src/Entity/Users.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cp;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pseudo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $photo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getCp(): ?int
+    {
+        return $this->cp;
+    }
+
+    public function setCp(int $cp): self
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getPhone(): ?int
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(int $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+}
